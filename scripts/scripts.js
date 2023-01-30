626e82f926d81fcac2415729a818ab55c9f7145f
Pablo Dario Robles <problesbpba@gmail.com>
2023-01-30 17:54:03 -0300

    Subiendo Curriculum Vitae

---
+++ scripts/scripts.js
@@ -0,0 +1,6 @@
+
+document.getElementById('boton').onclick = function(){
+    document.getElementById('datosPersonales').innerHTML = 
+    "Dni: 31658919, Domicilio: Avenida 4 N° 4282, Telefono: 2255-520010, mail: problesbpba@gmail.com"
+}
+
